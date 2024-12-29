363a75e3da854f961f089a398615de1758234acf
Kylie Bemis <k.bemis@northeastern.edu>
2024-12-28 20:04:33 -0700

    update testdb; add exampledb

--- tests/test_expdb.py
+++ tests/test_expdb.py
@@ -19,11 +19,11 @@ def test_db_open(database):
 
 def test_db_get(database):
 	with database as db:
-		assert db.get("Dataset-ID") == db["Dataset-ID"]
+		assert db.get("Test-data-01") == db["Test-data-01"]
 
 def test_db_ls(database):
 	with database as db:
-		assert "Dataset-ID" in db.ls()
+		assert "Test-data-01" in db.ls()
 
 def test_db_ls_cache(database):
 	with database as db:
@@ -31,21 +31,22 @@ def test_db_ls_cache(database):
 
 def test_db_search(database):
 	with database as db:
-		assert len(db.search("this dataset")) == 3
-		assert len(db.search("not")) == 1
+		assert len(db.search("bad wolf")) == 1
+		assert len(db.search("myself")) == 2
 		assert len(db.search("secret")) == 0
 
 def test_db_search_cache(database):
 	with database as db:
-		assert len(db.search_cache("this dataset")) == 0
-		assert len(db.search_cache("not")) == 0
+		assert len(db.search_cache("bad wolf")) == 0
+		assert len(db.search_cache("myself")) == 0
 		assert len(db.search_cache("secret")) == 0
 
 def test_db_describe(database):
 	with database as db:
-		assert db["Dataset-01"].describe() is not None
-		assert db["Dataset-02"].describe() is not None
-		assert db["Dataset-03"].describe() is not None
+		assert db["Test-data-01"].describe() is not None
+		assert db["Test-data-02"].describe() is not None
+		assert db["Test-data-03"].describe() is not None
+		assert db["Test-data-04"].describe() is not None
 
 def test_db_status(database):
 	with database as db:
