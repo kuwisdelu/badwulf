2575b2198ba839e743a9f85e8679e1d22fbae9c3
Kylie Bemis <k.bemis@northeastern.edu>
2025-08-30 23:35:57 -0400

    rearrange rssh tests

--- tests/test_rssh.py
+++ tests/test_rssh.py
@@ -6,6 +6,17 @@ import pytest
 from badwulf import rssh
 from badwulf.tools import *
 
+def test_rssh_with_gateway():
+	con = rssh("bad-wolf", "vortex",
+		server="login.dimension.time",
+		server_username="root")
+	assert con.username == "bad-wolf"
+	assert con.destination == "vortex"
+	assert con.hostname == "localhost"
+	assert con.server == "login.dimension.time"
+	assert con.server_username == "root"
+	assert not con.isopen()
+
 def test_rssh_without_gateway():
 	con = rssh("bad-wolf", "vortex")
 	assert con.username == "bad-wolf"
@@ -15,31 +26,28 @@ def test_rssh_without_gateway():
 	assert con.server_username is None
 	assert not con.isopen()
 
-def test_rssh_with_gateway():
-	con = rssh("bad-wolf", "vortex",
-		server="login.dimension.time",
-		server_username="root",
-		autoconnect=False)
-	assert con.username == "bad-wolf"
-	assert con.destination == "vortex"
-	assert con.hostname == "localhost"
-	assert con.server == "login.dimension.time"
-	assert con.server_username == "root"
-	assert not con.isopen()
-
-def test_rssh_upload_download_file():
+def test_rssh_download_file():
 	con = rssh(getenv("USER"), "localhost")
 	if con.isbatch():
 		tmpdir = tempfile.gettempdir()
-		tmp1 = os.path.join(tmpdir, "__badwulf_testfile__")
-		tmp2 = os.path.join(tmpdir, "__badwulf_testfile_download__")
-		tmp3 = os.path.join(tmpdir, "__badwulf_testfile_upload__")
+		tmp1 = os.path.join(tmpdir, "__badwulf_test_down")
+		tmp2 = os.path.join(tmpdir, "__badwulf_test_down_copy")
 		file_create(tmp1)
 		assert os.path.exists(tmp1)
 		con.download(tmp1, tmp2)
 		assert os.path.exists(tmp2)
-		con.upload(tmp2, tmp3)
-		assert os.path.exists(tmp3)
 		file_remove(tmp1)
 		file_remove(tmp2)
-		file_remove(tmp3)
+
+def test_rssh_upload_file():
+	con = rssh(getenv("USER"), "localhost")
+	if con.isbatch():
+		tmpdir = tempfile.gettempdir()
+		tmp1 = os.path.join(tmpdir, "__badwulf_test_up")
+		tmp2 = os.path.join(tmpdir, "__badwulf_test_up_copy")
+		file_create(tmp1)
+		assert os.path.exists(tmp1)
+		con.upload(tmp1, tmp2)
+		assert os.path.exists(tmp2)
+		file_remove(tmp1)
+		file_remove(tmp2)
