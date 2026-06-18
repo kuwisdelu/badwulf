be73ad886ac3ab59d9d7e24b26ff9dba690c9cd4
Kylie Bemis <k.bemis@northeastern.edu>
2026-06-17 23:02:23 -0400

    cli: update resolve_site()

--- src/badwulf/cli/sync.py
+++ src/badwulf/cli/sync.py
@@ -6,6 +6,7 @@ from .site import load_sites
 from .site import DEFAULT_SITE
 from .site import DEFAULT_HOST
 from .site import DEFAULT_PREFIX
+from .proj import resolve_project
 
 from ..db import projdb
 from ..util import prog_error
@@ -14,9 +15,30 @@ from ..util import mkpath
 from ..util import mktree
 from ..util import prune
 
-def fetch(args):
+def resolve_site(args):
 	sts = load_sites()
-	prog_error("NOT IMPLEMENTED YET", args)
+	if args.site is None:
+		site, host = DEFAULT_SITE, DEFAULT_HOST
+	else:
+		site, host = tokenize(args.site)
+		if host is None:
+			host = DEFAULT_HOST
+	if site not in sts:
+		prog_error(f"unknown site: {site}", args)
+	return sts, site, host
+
+def fetch(args):
+	sts, site, host = resolve_site(args)
+	prefix = DEFAULT_PREFIX if args.prefix is None else args.prefix
+	filename = f"manifest-{site}-{host}.json"
+	dst = os.path.join(sts[site].paths[prefix], filename)
+	sts.pull(site, 
+		src="manifest.json", 
+		dst=dst,
+		host_key=host,
+		path_key=prefix,
+		dry_run=args.dry_run,
+		ask=args.ask)
 
 def pull(args):
 	sts = load_sites()
@@ -30,14 +52,3 @@ def status(args):
 	sts = load_sites()
 	prog_error("NOT IMPLEMENTED YET", args)
 
-def parse_site(args):
-	sts = load_sites()
-	if args.site is None:
-		site, host = DEFAULT_SITE, DEFAULT_HOST
-	else:
-		site, host = tokenize(args.site)
-		if host is None:
-			host = DEFAULT_HOST
-	if site not in sts:
-		prog_error(f"unknown site: {site}", args)
-	return sts, site, host
